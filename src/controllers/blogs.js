6c68c34ed8749889d4f8f083c61b61fe470413a6
Erik Husgafvel <husgafvel.erik@gmail.com>
2023-12-20 23:23:27 +0800

    Tehtävä 13.6.

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -3,16 +3,27 @@ const router = require('express').Router();
 const Blog = require('../models/blogs');
 
 router.get('/', async (req, res) => {
-  const notes = await Blog.findAll();
-  res.json(notes);
+  const blogs = await Blog.findAll();
+  res.json(blogs);
 });
 
 router.post('/', async (req, res) => {
   try {
     const blog = await Blog.create(req.body);
-    return res.status(201).json(blog);
+    res.status(201).json(blog);
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog && req.body.likes >= 0) {
+    blog.likes = req.body.likes;
+    await blog.save();
+    res.status(200).json(blog);
+  } else {
+    res.status(400).end();
   }
 });
 
